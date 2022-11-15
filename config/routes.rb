4cfafde7959c16805f3a5c2bed8d32912c19a51c
miguel silva <miguelvdsilva@gmail.com>
2022-11-15 15:28:19 +0000

    ddd

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #list all
+  get "tasks", to: "tasks#index"
+
+  #create a new
+  get "tasks/new", to: "tasks#new", as: :new_task
+  # post a new task
+  post "tasks", to: "tasks#create"
+
+  # read one
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # update -> get
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # update -> patch
+  patch "tasks/:id", to: "tasks#update"
+
+  # delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
